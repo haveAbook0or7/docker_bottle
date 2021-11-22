251fde791cbd2ad157f5d224922e435fe7498c9b
haveAbook0or7 <haveabook.w@gmail.com>
2021-11-22 12:24:04 +0900

    ログイン関連途中。

--- src/api/userlogins.py
+++ src/api/userlogins.py
@@ -6,8 +6,6 @@ import urllib.request
 import base64
 import io
 from beaker.middleware import SessionMiddleware
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 def get_login_user():
     # セッションを取得
@@ -21,5 +19,43 @@ def get_login_user():
     return json.dumps({
         "message": None,
         "data": res
-    },
-    indent=4)
+    }, indent=4)
+
+def sign_in(key, payload, createnew=True):
+    postjson = json.load(payload)
+    res = {}
+    try:
+        conn = mysql.connector.connect(host="db", port=3306, user="db_user", password="pass", database="db_canvas")
+        cur = conn.cursor()
+        query = "SELECT * FROM User WHERE id LIKE %(id)s AND pass LIKE %(pass)s;"
+        cur.execute(query, postjson)
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        res = result
+        message = "OK"
+    except Exception as e:
+        message = e
+    
+    return json.dumps({
+        "message": message,
+        "data": res
+    }, indent=4)
+
+
+def sign_out():
+    # セッションを取得
+    session1 = bottle.request.environ.get('beaker.session')
+    session1["user"] = "ゲスト"
+    session1.save()
+    res = {}
+    res["user"] = session1["user"]
+    return json.dumps({
+        "message": None,
+        "data": res
+    }, indent=4)
+
+def sign_up_pre():
+
+def sign_up():
---
+++ src/static/js/signin/signin-form.vue
@@ -0,0 +1,57 @@
+<template>
+	<div :style="variables">
+		
+    </div>
+</template>
+
+<script>
+module.exports = {
+	components: {
+		'my-canvas': httpVueLoader('./my-canvas.vue'),
+        'my-palette': httpVueLoader('./my-palette.vue'),
+		'my-control': httpVueLoader('./my-control.vue'),
+		'my-menu': httpVueLoader('./my-menu.vue'),
+    },
+	created() {
+		// ユーザー認証
+		// axios.get("/userlogins/getuser")
+		// .then(response => {
+		// 	console.log(response.data);
+		// 	this.loginUser = response.data.data.user;
+		// })
+		// .catch(function (error) {
+		// 	console.log(error);
+		// });
+	},
+	mounted() {
+		
+	},
+	computed: {
+		variables() {
+			return {
+				"--height": this.canvasheight+"px",
+			}
+		},
+	},
+	data: function () {
+		return {
+			loginUser: null,
+			baseheight: null,
+			canvasheight: null,
+		}
+	},
+	methods: {
+		
+	},
+}
+// export default { Node.jsじゃないから、これだとダメだった。 }
+</script>
+
+<style scoped>
+	*{
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 13px;
+	}
+</style>
